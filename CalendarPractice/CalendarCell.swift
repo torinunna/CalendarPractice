fa6c2776501e27375dffa209a665108e05aef624
torinunna <torinunna9588@gmail.com>
2022-12-18 00:39:31 +0900

    add monthLabel, previousMonth button, nextMonthButton, collectionView and create CalendarCell.swift

---
+++ CalendarPractice/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  CalendarPractice
+//
+//  Created by YUJIN KWON on 2022/12/18.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    
+}
--- CalendarPractice/ViewController.swift
+++ CalendarPractice/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    @IBOutlet weak var collectionVieww: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func previousMonth(_ sender: Any) {
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+    }
 }
 
