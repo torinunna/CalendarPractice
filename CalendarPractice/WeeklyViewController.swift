06cd8bcc5a7f4376b65e2ed366a6cdbd63be1690
torinunna <torinunna9588@gmail.com>
2022-12-19 04:24:18 +0900

    set identifier for dayOfMonth, implement setCellsView, setMonthView methods to WeeklyVC, add previousWeek and nextWeek actions

--- CalendarPractice/WeeklyViewController.swift
+++ CalendarPractice/WeeklyViewController.swift
@@ -12,13 +12,78 @@ class WeeklyViewController: UIViewController {
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var selectedDate = Date()
+    var totalSquares = [Date]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCellsView()
+        setMonthView()
     }
     
+    func setCellsView() {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func setMonthView() {
+        totalSquares.removeAll()
+        
+        var current = CalendarHelper().sundayForDate(date: selectedDate)
+        let nextSunday = CalendarHelper().addDays(date: current, days: 7)
+        
+        while (current < nextSunday) {
+            totalSquares.append(current)
+            current = CalendarHelper().addDays(date: current, days: 1)
+        }
+       
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate) + " " + CalendarHelper().yearString(date: selectedDate)
+        collectionView.reloadData()
+    }
+
     @IBAction func previousWeek(_ sender: Any) {
+        selectedDate = CalendarHelper().addDays(date: selectedDate, days: -7)
+        setMonthView()
     }
     
     @IBAction func nextWeek(_ sender: Any) {
+        selectedDate = CalendarHelper().addDays(date: selectedDate, days: 7)
+        setMonthView()
+    }
+    
+    override open var shouldAutorotate: Bool {
+        return false
     }
 }
+
+extension WeeklyViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        let date = totalSquares[indexPath.item]
+        cell.dayOfMonth.text = String(CalendarHelper().dayOfMonth(date: date))
+        
+        if date == selectedDate {
+            cell.backgroundColor = UIColor.systemGreen
+        } else {
+            cell.backgroundColor = UIColor.white
+        }
+        
+        return cell
+    }
+}
+
+extension WeeklyViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedDate = totalSquares[indexPath.item]
+        collectionView.reloadData()
+    }
+}
+
+
