a9c45b1853fae485e7bc235ea31cacc4c5af46d1
torinunna <torinunna9588@gmail.com>
2022-12-19 02:50:48 +0900

    create WeeklyVC and storyboard, embed navigationViewController, add segue from ViewController to WeeklyVC

---
+++ CalendarPractice/WeeklyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WeeklyViewController.swift
+//  CalendarPractice
+//
+//  Created by YUJIN KWON on 2022/12/19.
+//
+
+import UIKit
+
+class WeeklyViewController: UIViewController {
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func previousWeek(_ sender: Any) {
+    }
+    
+    @IBAction func nextWeek(_ sender: Any) {
+    }
+}
