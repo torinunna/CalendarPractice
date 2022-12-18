88ca60413a2b31981d7f87f82a2281e10dbc2973
torinunna <torinunna9588@gmail.com>
2022-12-18 21:15:43 +0900

    create CalendarHelper, implement setMonthView method and add previous/nextMonth actions

--- CalendarPractice/ViewController.swift
+++ CalendarPractice/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     var selectedDate = Date()
-    var totalSquare = [String]()
+    var totalSquares = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setCellsView()
+        setMonthView()
     }
     
     func setCellsView() {
@@ -27,11 +28,40 @@ class ViewController: UIViewController {
         let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         flowLayout.itemSize = CGSize(width: width, height: height)
     }
+    
+    func setMonthView() {
+        totalSquares.removeAll()
+        let daysInMonth = CalendarHelper().daysInMonth(date: selectedDate)
+        let firstDayOfMonth = CalendarHelper().firstOfMonth(date: selectedDate)
+        let startingSpaces = CalendarHelper().weekDay(date: firstDayOfMonth)
+        
+        var count: Int = 1
+        
+        while(count <= 42) {
+            if(count <= startingSpaces || count - startingSpaces > daysInMonth) {
+                totalSquares.append("")
+            } else {
+                totalSquares.append(String(count - startingSpaces))
+            }
+            count += 1
+        }
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate) + " " + CalendarHelper().yearString(date: selectedDate)
+        collectionView.reloadData()
+    }
 
     @IBAction func previousMonth(_ sender: Any) {
+        
+        selectedDate = CalendarHelper().minusMonth(date: selectedDate)
+        setMonthView()
     }
     
     @IBAction func nextMonth(_ sender: Any) {
+        selectedDate = CalendarHelper().plusMonth(date: selectedDate)
+        setMonthView()
+    }
+    
+    override open var shouldAutorotate: Bool {
+        return false
     }
 }
 
@@ -41,12 +71,12 @@ extension ViewController: UICollectionViewDelegate {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        totalSquare.count
+        totalSquares.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
-        cell.monthLabel.text = totalSquare[indexPath.item]
+        cell.monthLabel.text = totalSquares[indexPath.item]
         
         return cell
     }
